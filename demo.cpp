ce73ff0a7d0b974df24ef387a8d7d726e0496848
Algohercules <adarshrreso@gmail.com>
2025-03-13 15:52:52 +0530

    ...

---
+++ demo.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int x, y;
+    cin >> x;
+    cin >> y;
+    cout << x << "ok " << y;
+    return 0;
+}
+
+
+// v.emplace_back(4);
+
+// vector<pair<int,int>>vec;
+
+// v.push_back({1,2});
+// v.emplace_back(1,2);
+
+
+// vector<int> v(5,100);
+
+
+// vector<int> v(5);
+
+// vector<int>  v1(5,100);
+// vector<int>  v2(v1);
+
+
+// vector<int>::iterator it = v.begin();
+// it++;
+// cout<<*(it)<<" ";
+// }
+
