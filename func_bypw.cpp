a624564738c345ca5013a0bec9064338de527361
Algohercules <adarshrreso@gmail.com>
2025-04-03 08:03:06 +0530

    scope of variable

--- func_bypw.cpp
+++ func_bypw.cpp
@@ -1,10 +1,49 @@
 #include<iostream>
 using namespace std;
 
-void welcome(){
-    cout<< "welcome to pw" <<endl;
-}
+// void welcome(){
+//     cout<< "welcome to pw"
+// }
 
-int main(){
-     welcome();
-}
+// int main(){
+//      welcome();
+// }
+
+
+
+// int  welcome(){
+//     cout<< "welcome to pw" <<endl;
+//     return 1;
+// }
+
+// int main(){
+//     int ans;
+//      ans = welcome();
+//      cout << ans <<endl;
+// }
+
+// int add(int num1, int num2){
+//      int sum = num1 + num2;
+//      return sum;
+// }
+
+
+// int main(){
+
+//     int a;
+//     int b;
+//     int ans;
+//     cin >> a >> b;
+
+//     ans = add(a,b);
+    
+//     cout << ans <<endl;
+    
+//     return 0;
+// }
+
+
+
+//scope :--
+// part of program from where variable is accessible
+
