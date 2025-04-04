5f806af047e14cadd32abb63b24b889c770f88df
Algohercules <adarshrreso@gmail.com>
2025-04-04 23:28:38 +0530

    sum of no.s from 1 to n

--- revision.cpp
+++ revision.cpp
@@ -4,23 +4,27 @@ using namespace std;
 
 // print no.s from 1 to n:---
 
-int main(){
-    int n;
-    int i = 1;
-    cin >> n ;
-    while(i <= n){
-          cout << i;
-          i++;
-    }    
-}
-
-//sum of no.s from 1 to n:---
-
 // int main(){
 //     int n;
 //     int i = 1;
+//     cin >> n ;
 //     while(i <= n){
-//          sum = sum + i;
-
-//     }
+//           cout << i;
+//           i++;
+//     }    
 // }
+
+//sum of no.s from 1 to n:---
+
+int main(){
+    int i = 1;
+    int n;
+    int sum = 0;
+    
+    cin >> n;
+    
+    while( i<= n){
+         sum = sum + i;
+          i++ ;
+    }  
+}
