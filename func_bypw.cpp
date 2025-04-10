8f30dd9f88d1075613de532f2e6a5d921c0e4e01
Algohercules <adarshrreso@gmail.com>
2025-04-10 23:41:12 +0530

    gjhguj

--- func_bypw.cpp
+++ func_bypw.cpp
@@ -209,28 +209,28 @@ formal and actual parameters:---
 
 //checking prime number
 
-bool isPrime(int n){
+// bool isPrime(int n){
 
 
-       //divide ho gya hai, not a prime no.
-       for(int i=2; i<=n; i++){
-            if(n%i == 0){
-                  return 0;
-            }
-       }
-       return 1;
-}
+//        //divide ho gya hai, not a prime no.
+//        for(int i=2; i<=n; i++){
+//             if(n%i == 0){
+//                   return 0;
+//             }
+//        }
+//        return 1;
+// }
 
-int main(){
+// int main(){
       
-      int n;
-      cin >> n;
-      if(isPrime(n)){
-            cout << " is a prime number" <<endl;
-      }
-      else {
-            cout << "is not a prime no." <<endl;
-      }
-}
+//       int n;
+//       cin >> n;
+//       if(isPrime(n)){
+//             cout << " is a prime number" <<endl;
+//       }
+//       else {
+//             cout << "is not a prime no." <<endl;
+//       }
+// }
 
 //ab nahio karunga
